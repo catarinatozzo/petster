a81d4a78eb92f38da2986b2c112e1e94e5aa6971
Catarina Tozzo <catarinatozzobueno@gmail.com>
2025-09-20 22:51:57 -0300

    tests

--- app/config.py
+++ app/config.py
@@ -1,10 +1,10 @@
 import os
 
-DB_USER = os.getenv("DB_USER", "root")
-DB_PASS = os.getenv("DB_PASS", "rootpassword")
-DB_NAME = os.getenv("DB_NAME", "pets")
-DB_HOST = os.getenv("DB_HOST", "localhost")
-DB_PORT = os.getenv("DB_PORT", "3306")
+DB_USER = "root"
+DB_PASS = "rootpassword"
+DB_NAME = "pets"
+DB_HOST = "localhost"
+DB_PORT = "3306"
 
 database_url = (
     f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
